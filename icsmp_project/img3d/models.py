49572824e85f31de20777434798b73bf6508e06a
ravellys <lucas.ravellys@ufpe.br>
2020-10-31 03:12:02 -0300

    add caminho da img

--- icsmp_project/img3d/models.py
+++ icsmp_project/img3d/models.py
@@ -7,11 +7,16 @@ class Img3d(OrderedModel):
     nome = models.CharField(max_length=32)
     descricao = models.TextField(null=True, blank=True)
     trabalhos_utilizados = models.TextField(null=True, blank=True)
-    slug = models.SlugField(unique=True)
+    slug = models.SlugField(unique=True, null=True, blank=True)
     arquivo = models.FileField()
+    url = models.CharField(max_length=200, null=True, blank=True)
 
     class Meta(OrderedModel.Meta):
         pass
+
+    def save(self, *args, **kwargs):
+        self.url = self.arquivo.url
+        super(Img3d, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.nome
